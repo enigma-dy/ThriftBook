0e75cbc114796b0f02face52d4be1e09fb7fc6ba
enigma-dy <oluwagbengaadedire@gmail.com>
2025-02-05 18:56:26 +0100

    Update

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ app.use(
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.ORIGIN,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
     credentials: true,
   })
